95f293209483f9544298b72a58a9109aeffe566d
EloToJaa <elotoja@protonmail.com>
2025-06-12 20:09:53 +0200

    First commit

---
+++ lua/elotoja/plugins/formatting.lua
@@ -0,0 +1,73 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				svelte = { "prettierd" },
+				astro = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
+				liquid = { "prettierd" },
+				bash = { "shfmt" },
+				lua = { "stylua" },
+				nix = { "alejandra" },
+				rust = { "rustfmt" },
+				sql = { "sqlfmt" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				go = { "gofumpt" },
+				python = { "ruff_organize_imports" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+			formatters = {
+				ruff_organize_imports = {
+					command = "ruff",
+					args = {
+						"check",
+						"--force-exclude",
+						"--select=I001",
+						"--fix",
+						"--exit-zero",
+						"--stdin-filename",
+						"$FILENAME",
+						"-",
+					},
+					stdin = true,
+					cwd = require("conform.util").root_file({
+						"pyproject.toml",
+						"ruff.toml",
+						".ruff.toml",
+					}),
+				},
+			},
+		})
+
+		vim.keymap.set(
+			{ "n", "v" },
+			"<leader>mp",
+			function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 4000,
+				})
+			end,
+			{ desc = "Format file or range (in visual mode)" }
+		)
+	end,
+}
